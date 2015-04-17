389c1d80d3fd24519365b51e7d20c1d45e0c46c6
Ryan Knight <ryan@grandcloud.com>
2015-04-17 14:03:56 -0500

    more setup stuff

--- vagrant/contributions/techsupply/4.post.sh
+++ vagrant/contributions/techsupply/4.post.sh
@@ -19,19 +19,19 @@ shift
 done
 
 SINGLE_SEED="${SEEDS%%,*}"
-cqlsh $SINGLE_SEED -f /cornerstone/cql/datastax/black-friday/retail.cql
+cqlsh $SINGLE_SEED -f /cornerstone/cql/contributions/techsupply/techsupply.cql
 
-#/cornerstone/scripts/datastax/black-friday/1.seed_zipcode_data/1.zipcodes-to-cassandra.py
+#/cornerstone/scripts/contributions/techsupply/1.seed_zipcode_data/1.zipcodes-to-cassandra.py
 
-/cornerstone/scripts/datastax/black-friday/2.seed_retail_data/1.download-data.sh
-/cornerstone/scripts/datastax/black-friday/2.seed_retail_data/2.data-to-cassandra.py
+/cornerstone/scripts/contributions/techsupply/2.seed_retail_data/1.download-data.sh
+/cornerstone/scripts/contributions/techsupply/2.seed_retail_data/2.data-to-cassandra.py
 
-/cornerstone/scripts/datastax/black-friday/3.scan_data/1.extract-ids.py
-#/cornerstone/scripts/datastax/black-friday/3.scan_data/2.extract-zipcodes.py
-/cornerstone/scripts/datastax/black-friday/3.scan_data/3.start-metagener.sh
+/cornerstone/scripts/contributions/techsupply/3.scan_data/1.extract-ids.py
+#/cornerstone/scripts/contributions/techsupply/3.scan_data/2.extract-zipcodes.py
+/cornerstone/scripts/contributions/techsupply/3.scan_data/3.start-metagener.sh
 sleep 20
-/cornerstone/scripts/datastax/black-friday/3.scan_data/4.metagener-to-cassandra-stores-employees.py
+/cornerstone/scripts/contributions/techsupply/3.scan_data/4.metagener-to-cassandra-stores-employees.py
 
 mkdir -p /mnt/log/spark_streaming/
 nohup nc -l 5005 &
-nohup /cornerstone/scripts/datastax/black-friday/3.scan_data/5.metagener-to-cassandra-scan-items.py &
+nohup /cornerstone/scripts/contributions/techsupply/3.scan_data/5.metagener-to-cassandra-scan-items.py &
